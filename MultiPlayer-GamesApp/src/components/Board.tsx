1c0ad2e1f726e576ab09c7e5cfa4202f633df11c
Anwesh Peddineni <anweshpeddineni@Anweshs-MacBook-Pro.local>
2023-07-23 17:56:01 -0400

    Updated the calculate win function to work differently based on board size

--- MultiPlayer-GamesApp/src/components/Board.tsx
+++ MultiPlayer-GamesApp/src/components/Board.tsx
@@ -4,7 +4,7 @@ import Square from "./Square";
 
 export default function Board() {
     
-    let n = 3;
+    let n = 5;
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(n).fill("").map(() => Array(n).fill("")));
 
@@ -36,78 +36,101 @@ export default function Board() {
       }
     }
 
-    return(
-        <>
-        {gameWinner ? <p>Winner is {gameWinner} </p> : <p> {xIsNext ? "X turn" : "O turn" } </p>}
-        {squares.map((row, rowIndex) => (
-        <div key={rowIndex}>
-        {row.map((element, colIndex) => (
-          <Square
-            key={`${rowIndex}-${colIndex}`}
-            value={element}
-            onSquareClick={() => handleClick(rowIndex, colIndex)}
-          />
-        ))}
+    return (
+      <div className="container mt-4">
+        {gameWinner ? (
+          <p className="lead">Winner is {gameWinner}</p>
+        ) : (
+          <p className="lead">{xIsNext ? "X turn" : "O turn"}</p>
+        )}
+        <div className="row justify-content-center">
+          <div className="row-3">
+            <div className="row border border-dark">
+              {squares.map((row, rowIndex) => (
+                <div className="col-5 border border-dark" key={rowIndex}>
+                  {row.map((element, colIndex) => (
+                    <Square
+                      key={`${rowIndex}-${colIndex}`}
+                      value={element}
+                      onSquareClick={() => handleClick(rowIndex, colIndex)}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-    ))}
-        </>
-    )
-}
+    );
+  }
 
 
 
-// TODO: Explain the better way to do this type magic?
+
 function calculateWinner(squares: string[][]): string | null{
 
     let n = squares.length;
+    let l;
+
+    if(n<4){
+      l = n;
+    }else{
+      l = 4;
+    }
 
     // row win
 
-    for(let i=0; i<n; i++){
-        if(squares[i].every((square)=> square === "X")){
-            return "X";
-        }else if(squares[i].every((square)=> square === "O")){
-            return "O";
-        }else if(squares[i].every((square)=> square === null)){
-            return null;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - l + 1; j++) {
+        const consecutiveSquares = squares[i].slice(j, j + l);
+        if (consecutiveSquares.every((square) => square === "X")) {
+          return "X";
+        } else if (consecutiveSquares.every((square) => square === "O")) {
+          return "O";
+        } else if (consecutiveSquares.every((square) => square === null)) {
+          return null;
         }
+      }
     }
 
     // col win
 
-    for(let i=0; i<n; i++){
-        if(squares.every((row) => row[i] === "X")){
+    for(let i=0; i<n; i++) {
+      for(let j = 0; j < n - l + 1; j++){
+        const consecutiveSquares = squares.slice(j, j + l);
+        if(consecutiveSquares.every((row) => row[i] === "X")){
             return "X";
-        }else if(squares.every((row) => row[i] === "O")){
+        }else if(consecutiveSquares.every((row) => row[i] === "O")){
             return "O";
-        }else if(squares.every((row) => row[i] === null)){
+        }else if(consecutiveSquares.every((row) => row[i] === null)){
             return null;
         }
+      }
     }
 
     // diagonal win
-    let hasDiagonalWin = true;
-    for(let i=0; i<n; i++){
-        if(squares[i][i] !== squares[0][0]){
-            hasDiagonalWin = false;
-            break;
-        }
-    }
-    if (hasDiagonalWin) {
-        return squares[0][0];
+    for (let i = 0; i < n - l + 1; i++) {
+      const consecutiveSquares = squares.slice(i, i + l).map((row, idx) => row[i + idx]);
+      if (consecutiveSquares.every((square) => square === "X")) {
+        return "X";
+      } else if (consecutiveSquares.every((square) => square === "O")) {
+        return "O";
+      } else if (consecutiveSquares.every((square) => square === null)) {
+        return null;
+      }
     }
 
     // anti-diagonal win
-    let hasAntiDiagonalWin = true;
-    for (let i = 0; i < n; i++) {
-        if (squares[i][n - 1 - i] !== squares[0][n - 1]) {
-            hasAntiDiagonalWin = false;
-            break;
-        }
-    }
-    if (hasAntiDiagonalWin) {
-    return squares[0][n - 1];
-    }
+   for (let i = 0; i < n - l + 1; i++) {
+  const consecutiveSquares = squares.slice(i, i + l).map((row, idx) => row[n - 1 - i - idx]);
+  if (consecutiveSquares.every((square) => square === "X")) {
+    return "X";
+  } else if (consecutiveSquares.every((square) => square === "O")) {
+    return "O";
+  } else if (consecutiveSquares.every((square) => square === null)) {
+    return null;
+  }
+  }
 
     // tie
     if(squares.every((row) => row.every((square) => square !== ""))){
